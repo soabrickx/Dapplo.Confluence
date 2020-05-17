4d290d7b655db1e65838ac7e19888a793af3d90a
Krom, Robertus <Robertus.Krom@ing.de>
2020-05-17 23:59:24 +0200

    Using HttpCode.Forbidden to specify that a label does not exist, is nasty...

--- src/Dapplo.Confluence/UserExtensions.cs
+++ src/Dapplo.Confluence/UserExtensions.cs
@@ -295,7 +295,7 @@ namespace Dapplo.Confluence
 
             confluenceClient.Behaviour.MakeCurrent();
             var response = await userWatchLabelUri.GetAsAsync<HttpResponse<UserWatch>>(cancellationToken).ConfigureAwait(false);
-            return response.HandleErrors(HttpStatusCode.OK, HttpStatusCode.NotFound)?.IsWatching ?? false;
+            return response.HandleErrors(HttpStatusCode.OK, HttpStatusCode.Forbidden)?.IsWatching ?? false;
         }
 
         /// <summary>
